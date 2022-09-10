73446da5a2c8096819329e1ebf55b44801fa3526
Code-rina <katerina.kreibich@gmail.com>
2022-09-10 02:47:46 -0700

    navBar basic done

--- python-project-starter/react-app/src/components/NavBar.js
+++ python-project-starter/react-app/src/components/NavBar.js
@@ -1,39 +1,52 @@
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
 
 const NavBar = () => {
+
+  const sessionUser = useSelector((state) => state.session?.user)
+
+
   return (
-  <>
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+    <nav className="navbar-main-container">
+      <div className="navbar-sub-container">
+        <div className="navbar-div">
+          <NavLink className="nav-logo" to='/' exact={true} activeClassName='active'>
+            SustainMe
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+        {/* </div> */}
+        {/* <div className="navbar-right-div"> */}
+          <div className="nav-li">
+            <NavLink className="nav-items" to='/about' exact={true} activeClassName='active'>
+              About Us
+            </NavLink>
+          </div>
+        {/* {sessionUser &&  */}
+              <div className="nav-li">
+                <NavLink className="nav-items" to='/events' exact={true} activeClassName='active'>
+                  Events
+                </NavLink>
+              </div>
+              <div className="nav-li">
+                <NavLink className="nav-items" to='/sign-up' exact={true} activeClassName='active'>
+                  Sign Up
+                </NavLink>
+              </div>
+              <div className="nav-li">
+                <NavLink className="nav-items" to='/users' exact={true} activeClassName='active'>
+                  Users
+                </NavLink>
+              </div>
+              <div className="nav-li">
+                <LogoutButton />
+              </div>
+            </div>
+        {/* } */}
+      </div>
     </nav>
-  </>
   );
 }
 
